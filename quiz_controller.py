ddcb44cbdbd74271dc17a502c96186d47e49fa1d
unknown <ramosyazmien2@gmail.com>
2025-05-28 13:00:38 +0800

    Quiz logic

--- quiz_controller.py
+++ quiz_controller.py
@@ -1,16 +1,17 @@
 class QuizController:
-    def __init__(self, questions):
+    #Handles quiz flow, question tracking, and score
+    def __init__(self, questions):#Initialize with list of questions
         self.questions = questions
         self.current_index = 0
         self.score = 0
 
-    def get_current_question(self):
+    def get_current_question(self): #Return current question
         return self.questions[self.current_index]
 
-    def get_correct_answer(self):
+    def get_correct_answer(self): #Return correct answer key (a/b/c/d)
         return self.questions[self.current_index]['correct_answer']
 
-    def get_correct_answer_text(self):
+    def get_correct_answer_text(self): #Return text of correct answer
         correct_key = self.get_correct_answer()
         return self.questions[self.current_index]['choices'][correct_key]
 
@@ -22,3 +23,4 @@ class QuizController:
 
     def is_finished(self):
         return self.current_index >= len(self.questions)
+
