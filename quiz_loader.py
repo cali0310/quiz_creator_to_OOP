5f4da01c74d30a47613d9b1e3dfe41030b04dae6
unknown <ramosyazmien2@gmail.com>
2025-05-28 13:18:32 +0800

    Loads questions from JSON file

---
+++ quiz_loader.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+class QuestionLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as file:
+                return json.load(file)
+        return []
